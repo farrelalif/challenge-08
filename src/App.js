3f6861ee48496ab07c549f67504c07faff95ae30
wlmchrstn <wlmchrstn@gmail.com>
2022-04-25 23:01:09 +0700

    Add initial state for todo list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Bikin fitur create
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    useEffect(() => {
+        const initialState = ['Nyapu Lantai', 'Coding Challenge', 'Bersih-bersih'];
+        sessionStorage.setItem('todolist', JSON.parse(initialState));
+    },[]);
+    return (
+        <div className="App">
+            <ul>
+                <li>
+                    todo list 1
+                </li>
+                <li>
+                    todo list 2
+                </li>
+                <li>
+                    todo list 3
+                </li>
+            </ul>
+        </div>
+    );
+};
 
 export default App;
