45f6586fcc450b98080dc79e7f1634444c1510f4
wlmchrstn <wlmchrstn@gmail.com>
2022-05-07 12:18:10 +0700

    [will] add crud function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import React, { useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import './reset.scss';
+import './App.scss';
 
 function App() {
+    const [loading, setLoading] = useState(true);
+    const [todo, setTodo] = useState('');
+    const [list, setList] = useState();
+
     useEffect(() => {
-        const initialState = ['Nyapu Lantai', 'Coding Challenge', 'Bersih-bersih'];
-        sessionStorage.setItem('todolist', JSON.parse(initialState));
-    },[]);
+        const item = sessionStorage.getItem('todolist');
+        setList(JSON.parse(item));
+        setLoading(false);
+    }, [list]);
+
+    // Function Get Todo List
+    const getTodoList = () => {
+        if (!list) return null;
+        return list.map((value, index) => {
+            return (
+                <div key={index} className={'list-container'}>
+                    <p className={'list-paragraph'}>
+                        {value}
+                    </p>
+                    <button onClick={() => deleteTodoList(index)}>
+                        {'Delete'}
+                    </button>
+                </div>
+            );
+        });
+    };
+
+    // Function Tambah Todo List
+    const createTodoList = (params) => {
+        if (!list) {
+            sessionStorage.setItem('todolist', JSON.stringify([params]));
+            return setList([params])
+        }
+        list.push(params);
+        sessionStorage.setItem('todolist', JSON.stringify(list));
+        return setList(list);
+    };
+
+    // Function Update Todo List
+    const updateTodoList = (index, value) => {
+        list.splice(index, 1, value);
+        sessionStorage.setItem('todolist', JSON.stringify(list));
+        return setList(list);
+    };
+
+    // Function Delete Todo List
+    const deleteTodoList = (index) => {
+        list.splice(index, 1);
+        sessionStorage.setItem('todolist', JSON.stringify(list));
+        return setList(list);
+    };
+
+    if (loading) return <p>Loading</p>;
+
     return (
-        <div className="App">
-            <ul>
-                <li>
-                    todo list 1
-                </li>
-                <li>
-                    todo list 2
-                </li>
-                <li>
-                    todo list 3
-                </li>
-            </ul>
+        <div className={'root'}>
+            <form onSubmit={() => createTodoList(todo)} >
+                <input onChange={(e) => setTodo(e.target.value)} />
+                <button type='submit'>
+                    {'Add'}
+                </button>
+            </form>
+            {getTodoList()}
         </div>
     );
 };
